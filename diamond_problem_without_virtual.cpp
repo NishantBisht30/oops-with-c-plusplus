9a5992294f6c1cc3d39c9a185be9182fea7b7b97
Nishant Bisht <91425692+ashbishtb@users.noreply.github.com>
2021-12-07 14:40:39 +0530

    Create diamond_problem_without_virtual.cpp

---
+++ diamond_problem_without_virtual.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class A
+{
+
+    public:
+        int k;
+        void display(){
+            cout<<"This is display meathod for class A"<<endl;
+        }
+};
+class B : public A
+{
+    public:
+        void display1(){
+            cout<<"This is display meathod for class B"<<endl;
+        }
+};
+
+
+class C : public A
+{
+public:
+    void display2(){
+            cout<<"This is display meathod for class c"<<endl;
+    }
+};
+
+
+class D : public B, public C
+{
+public:
+    void display3(){
+            cout<<"This is display meathod for class D"<<endl;
+    }
+};
+
+int main()
+{
+    D d;
+    d.display();
+    d.display1();
+    d.display2();
+    d.display3();
+
+d.k = 10;     // will show : memeber display was ambiguous
+    return 0;
+}
